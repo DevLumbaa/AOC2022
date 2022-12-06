d65cd075c17dedac3eaee6da3722ee752d8797b3
DevLumbaa <52379118+DevLumbaa@users.noreply.github.com>
2022-12-06 22:29:05 +0000

    puzzle3 part 2

--- solutions/puzzle3.py
+++ solutions/puzzle3.py
@@ -12,12 +12,20 @@ letter_point_dict = dict(zip(list_letters, list_points))
 with open(filename, 'r') as file:
     lines = file.read().splitlines()
     points = 0
-    for line in lines:
-        data = line.strip()
-        firstpart, secondpart = data[:len(data)//2], data[len(data)//2:]
-        misplaced_itmes = set.intersection(set(firstpart),set(secondpart))
-        for item in misplaced_itmes:
-            points += letter_point_dict.get(item)
+    lines_subset  =  list(zip(*([iter(lines)]*3)))
+# second part
+    for item in lines_subset:
+        badge = set.intersection(set(item[0]), set(item[1]), set(item[2]))
+        for val in badge:
+            points += letter_point_dict.get(val)
+
+#first part
+    # for line in lines:
+    #     data = line.strip()
+    #     firstpart, secondpart = data[:len(data)//2], data[len(data)//2:]
+    #     misplaced_itmes = set.intersection(set(firstpart),set(secondpart))
+    #     for item in misplaced_itmes:
+    #         points += letter_point_dict.get(item)
     
     print(points)
 
