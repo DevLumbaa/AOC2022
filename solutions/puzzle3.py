0140b19bfca62ed6f39bb90d0772bb078771ec1b
DevLumbaa <52379118+DevLumbaa@users.noreply.github.com>
2022-12-06 17:08:45 +0000

    first part solved

--- solutions/puzzle3.py
+++ solutions/puzzle3.py
@@ -3,18 +3,22 @@ from pathlib import Path
 import string
 
 cur_dir, _ = os.path.split(os.path.abspath(__file__))
-filename =  str((Path(cur_dir).parents[0])) + '\inputs\puzzle3_dummy_data.txt'
+filename =  str((Path(cur_dir).parents[0])) + '\inputs\puzzle3.txt'
 
 list_letters = list(string.ascii_letters)
 list_points = list(range(1,53,1))
 letter_point_dict = dict(zip(list_letters, list_points))
-print(letter_point_dict)
 
 with open(filename, 'r') as file:
     lines = file.read().splitlines()
+    points = 0
     for line in lines:
         data = line.strip()
         firstpart, secondpart = data[:len(data)//2], data[len(data)//2:]
-        print(firstpart, secondpart)
+        misplaced_itmes = set.intersection(set(firstpart),set(secondpart))
+        for item in misplaced_itmes:
+            points += letter_point_dict.get(item)
+    
+    print(points)
 
 file.close()
